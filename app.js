61bdd0ce6c5aa94643d70e989a73abd97b5d5bc5
Sikandar Ali <sikandarali94@hotmail.com>
2019-10-31 22:10:06 +1100

    hooked up morgan message logger with express

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const daysRoute = require('./api/routes/days');
 const timeslotsRoute = require('./api/routes/timeslots');
 const bookRoute = require('./api/routes/book');
 
 const app = express();
+
+app.use(morgan('dev'));
 
 app.use('/days', daysRoute);
 app.use('/timeslots', timeslotsRoute);
